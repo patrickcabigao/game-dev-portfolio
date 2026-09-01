a4eca9134f0d8e4b4cf760c455618cb179f2c9d3
Patrick Cabigao <plcabigao@gmail.com>
2026-08-31 20:49:51 -0700

    contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,12 +1,32 @@
 import Page from './Page';
-import itchLogo from '../assets/itchio-textless-white.svg';
+import itchLogo from '../assets/itchio-logo.svg';
+import githubLogo from '../assets/github-logo.svg';
+import linkedinLogo from '../assets/linkedin-logo.svg';
 
 export default function Contact(): React.ReactNode {
     return (
         <Page>
             <h1>Contact</h1>
-            <p>For contacting me directly:</p>
-            <img src={itchLogo} alt="itch.io logo"/>
+            <p>For contacting me directly, please email <a href="mailto:patrickcabigao@ucsb.edu">patrickcabigao@ucsb.edu</a>.</p>
+            <br/>
+            <p>You may also find me on these sites:</p>
+
+            <ul className="contact-sites">
+                <li>
+                    <img src={itchLogo} alt="itch.io logo"/>
+                    <a href="https://eleven-scute-turtle.itch.io/">itch.io</a>
+                </li>
+
+                <li>
+                    <img src={githubLogo} alt="GitHub logo"/>
+                    <a href="https://github.com/patrickcabigao">GitHub</a>
+                </li>
+
+                <li>
+                    <img src={linkedinLogo} alt="LinkedIn logo"/>
+                    <a href="https://www.linkedin.com/in/patrick-cabigao-a4560629b/">LinkedIn</a>
+                </li>
+            </ul>
         </Page>
     );
 }

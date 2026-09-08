4536da88fcf627f402b418ac38c43b40abe3e8d0
Patrick Cabigao <plcabigao@gmail.com>
2026-09-08 13:43:47 -0700

    quick fix

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,5 +1,5 @@
 import Navbar from "./Navbar";
-import Footer from "./Footer";
+// import Footer from "./Footer";
 
 const Page = ({ children }: {children: React.ReactNode}): React.ReactNode => {
     return (
